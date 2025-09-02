1fff30e4a85b868d61ba4e1ada46ffbeeaa3b294
Suraj Singh <suraj.job.2002@gmail.com>
2025-09-02 19:31:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ function isSameType(value1, value2) {
 		return true;
 	
 	}
+	if (Number.isNaN(value1) || Number.isNaN(value2)) {
+    return false;
+  }
 	return typeof(value1)===typeof(value2)
 	
 }
